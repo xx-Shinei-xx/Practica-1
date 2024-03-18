204e5941b14c7ee79366a8a1a0b9fc9667422599
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-17 23:08:50 -0600

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -3,6 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import binom
 
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url('background_image.jpg') no-repeat center center fixed;
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
 # Datos de las caras de las monedas
 listas = {
     "Guillermo y Shawn": [2, 4, 5, 3, 7, 3, 4, 6, 4, 4, 3, 5, 3, 2, 3, 4, 8, 6, 4, 2, 5, 5, 3, 8, 4, 7, 4, 6, 3, 5, 8, 7, 3, 3, 6, 5, 4, 4, 5, 2, 5, 3, 7, 6, 3, 6, 5, 2, 4, 6, 5, 4, 6, 3, 6, 5, 3, 7, 8, 7, 4, 4, 4, 8, 5, 4, 3, 5, 7, 5, 2, 2, 3, 5, 1, 6, 4, 6, 4, 4, 3, 3, 6, 6, 3, 4, 5, 5, 5, 7, 6, 7, 4, 3, 5, 4, 5, 7, 6, 5],
